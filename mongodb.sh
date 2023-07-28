58cd4e592a4663a7757fdea7e73a8077c93c3800
Benarjee <benarjeechitekala@hotmail.com>
2023-07-27 21:21:51 -0400

    Adding Log Outputs & Changing IP Address

--- mongodb.sh
+++ mongodb.sh
@@ -5,7 +5,7 @@ echo -e "\e[36m>>>>>>>>>>>>>>>> Installing Mongodb  >>>>>>>>> \e[0m"
 yum install mongodb-org -y &>>/tmp/roboshp.log
 
 echo -e "\e[36m>>>>>>>>>>>>>>>> Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf >>>>>>>>> \e[0m"
-sed -i 's/127.0.0.1/0.0.0.0/'
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 echo -e "\e[36m>>>>>>>>>>>>>>>> Enabling Mongod >>>>>>>>> \e[0m"
 systemctl enable mongod &>>/tmp/roboshop.log
--- redis.sh
+++ redis.sh
@@ -3,6 +3,6 @@ yum module enable redis:remi-6.2 -y
 yum install redis -y
 echo -e "\e[36m>>>>>>> Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/redis.conf & /etc/redis/redis.conf >>>\e[0m"
 echo -e "\e[36m>>>>>>>>>>>>>>>>> Downloading Nodejs >>>>>>>>>>\e[0m"
-sed -i 's/127.0.0.1/0.0.0.0/'
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf  /etc/redis/redis.conf
 systemctl enable redis
 systemctl restart redis
