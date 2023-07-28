c8a43d9e029cb0e3ccb7b5e4acea2101ceb52f9c
Benarjee <benarjeechitekala@hotmail.com>
2023-07-27 20:57:47 -0400

    Adding Log Outputs & Changing IP Address

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,14 @@
+echo -e "\e[36m>>>>>>>>>>>>>>>> Creating Mongo Repo File >>>>>>>>> \e[0m"
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-yum install mongodb-org -y
-# Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
-systemctl enable mongod
-systemctl restart mongod
+echo -e "\e[36m>>>>>>>>>>>>>>>> Installing Mongodb  >>>>>>>>> \e[0m"
+yum install mongodb-org -y &>>/tmp/roboshp.log
+
+echo -e "\e[36m>>>>>>>>>>>>>>>> Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf >>>>>>>>> \e[0m"
+sed -i 's/127.0.0.1/0.0.0.0/'
+
+echo -e "\e[36m>>>>>>>>>>>>>>>> Enabling Mongod >>>>>>>>> \e[0m"
+systemctl enable mongod &>>/tmp/roboshop.log
+
+echo -e "\e[36m>>>>>>>>>>>>>>>> Restart  Mongod >>>>>>>>> \e[0m"
+systemctl restart mongod &>>/tmp/roboshop.log
--- redis.sh
+++ redis.sh
@@ -1,6 +1,8 @@
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 yum module enable redis:remi-6.2 -y
 yum install redis -y
-# Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/redis.conf & /etc/redis/redis.conf
+echo -e "\e[36m>>>>>>> Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/redis.conf & /etc/redis/redis.conf >>>\e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>>>> Downloading Nodejs >>>>>>>>>>\e[0m"
+sed -i 's/127.0.0.1/0.0.0.0/'
 systemctl enable redis
 systemctl restart redis
