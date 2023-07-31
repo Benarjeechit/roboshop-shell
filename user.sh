b02829e54b797fda190717082fd19e45151fea1d
Benarjee <benarjeechitekala@hotmail.com>
2023-07-31 15:22:42 -0400

    Recheck

--- user.sh
+++ user.sh
@@ -1,5 +1,21 @@
-component=user
-source common.sh
+#component=user
+#source common.sh
 
-func_nodejs
+#func_nodejs
+cp user.service /etc/systemd/system/user.service
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+rm -rf /app
+mkdir /app
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+cd /app
+unzip /tmp/user.zip
+cd /app
+npm install
+systemctl daemon-reload
+systemctl enable user
+systemctl start user
+yum install mongodb-org-shell -y
+mongo --host mongodb.benzdevops.online </app/schema/user.js
 
