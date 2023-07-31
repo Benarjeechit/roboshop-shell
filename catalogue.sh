7acd1a34e54f241c86f52c0af851f87a30391cdb
Benarjee <benarjeechitekala@hotmail.com>
2023-07-31 14:02:35 -0400

    Recheck

--- catalogue.sh
+++ catalogue.sh
@@ -6,6 +6,7 @@ cp catlogue.service /etc/systemd/system/catalogue.service
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 useradd roboshop
+rm -rf /app
 mkdir /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
